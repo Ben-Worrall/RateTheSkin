d028ba2c6a0bc327bef117cf9dd7b7a4da3b9528
Ben <benworrallpc@gmail.com>
2023-05-12 18:26:48 +0200

    change filename

--- csgskins.js
+++ csgskins.js
@@ -1685,7 +1685,7 @@ document.getElementById("DownloadResult").addEventListener('click', function () 
             var anchorTag = document.createElement("a");
             document.body.appendChild(anchorTag);
             document.getElementById("previewImg").appendChild(canvas);			
-            anchorTag.download = "filename.jpg";
+            anchorTag.download = "YourRatingNumer.jpg";
             anchorTag.href = canvas.toDataURL();
             anchorTag.target = '_blank';
             anchorTag.click();
